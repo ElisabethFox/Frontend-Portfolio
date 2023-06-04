dd38d55e2e3e81bf8bfb9ec482ad8da0a63260e7
ElisabethFox <lisa.black.fox17@gmail.com>
2023-06-04 17:38:38 +0300

    delete useRef

--- src/components/btnDarkMode/BtnDarkMode.js
+++ src/components/btnDarkMode/BtnDarkMode.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { useLocalStorage } from '../../utils/useLocalStorage';
 
 import detectDarkMode from '../../utils/detectDarkMode';
@@ -9,26 +9,35 @@ import moon from './moon.svg';
 
 const BtnDarkMode = () => {
     const [darkMode, setDarkMode] = useLocalStorage('darkMode', detectDarkMode());
-    const btnRef = useRef(null);
 
     useEffect(() => {
         if (darkMode === 'dark') {
             document.body.classList.add('dark');
-            btnRef.current.classList.add('dark-mode-btn--active');
         } else {
             document.body.classList.remove('dark');
-            btnRef.current.classList.remove('dark-mode-btn--active');
         }
     },[darkMode]);
+
+    useEffect(() => {
+        window
+            .matchMedia('(prefers-color-scheme: dark)')
+            .addEventListener('change', (event) => {
+                const newColorScheme = event.matches ? 'dark' : 'light';
+                setDarkMode(newColorScheme);
+            });
+    }, [setDarkMode]);
 
     const toggleDarkMode = () => {
         setDarkMode((currentValue) => {
             return currentValue === 'light' ? 'dark' : 'light';
         });
-    }
+    };
+
+    const btnNormal ='dark-mode-btn';
+    const btnActive ='dark-mode-btn dark-mode-btn--active';
 
     return ( 
-    <button ref={btnRef} className="dark-mode-btn" onClick={toggleDarkMode}>
+    <button className={darkMode === 'dark' ? btnActive : btnNormal} onClick={toggleDarkMode}>
         <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
         <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
     </button>
